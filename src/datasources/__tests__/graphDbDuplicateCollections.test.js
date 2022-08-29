a4c66bd7190098dd10a12b0b9034e4edadcb9cc1
Ed Olivares <34591886+eudoroolivares2016@users.noreply.github.com>
2022-08-29 13:22:49 -0400

    CMR-8424: change userId in duplicate collections to edlUsername; alter tests for verifying jwt token
    JWT token verification locally was having an issue passing one test due to the kid value not being supplied; this case cannot happen as this is meant to be an JWK token that is stored locally

--- src/datasources/__tests__/graphDbDuplicateCollections.test.js
+++ src/datasources/__tests__/graphDbDuplicateCollections.test.js
@@ -4,7 +4,7 @@ import graphDbDuplicateCollectionsDatasource from '../graphDbDuplicateCollection
 
 import duplicatedCollectionsGraphdbResponseMocks from './__mocks__/duplicateCollections.graphdbResponse.mocks'
 import duplicateCollectionsRelatedUrlTypeResponseMocks from './__mocks__/duplicateCollections.response.mocks'
-// TODO: I need to add a uuid that gets passed to all of these tests
+
 describe('graphDb', () => {
   const OLD_ENV = process.env
 
@@ -35,8 +35,7 @@ describe('graphDb', () => {
         })
         .post(() => true)
         .reply(200, duplicatedCollectionsGraphdbResponseMocks)
-        // TODO: how can I grab this value that is being posted out and parse it
-        // TODO: delete this later
+
       // Mock the EDL call to retrieve the client's permitted groups
       nock(/example/)
         .get(/groups_for_user/)
@@ -72,7 +71,7 @@ describe('graphDb', () => {
           }
         },
         { 'CMR-Request-Id': 'abcd-1234-efgh-5678' },
-        'someEdlUsername' // TODO: I put this uuid here since it is getting passed from the context
+        'someEdlUsername'
       )
 
       expect(response).toEqual(duplicateCollectionsRelatedUrlTypeResponseMocks)
@@ -86,7 +85,7 @@ describe('graphDb', () => {
           doi: null
         },
         { 'CMR-Request-Id': 'abcd-1234-efgh-5678' },
-        'someEdlUsername' // TODO: UUID from the context
+        'someEdlUsername'
       )
 
       expect(response).toEqual({
@@ -148,7 +147,7 @@ describe('graphDb', () => {
         }
       },
       { 'CMR-Request-Id': 'abcd-1234-efgh-5678' },
-      'someEdlUsername' // TODO: I put this uuid here since it is getting passed from the context
+      'someEdlUsername'
     )
 
     expect(response).toEqual(duplicateCollectionsRelatedUrlTypeResponseMocks)
@@ -161,7 +160,7 @@ describe('graphDb', () => {
         errors: ['HTTP Error']
       }, {
         'cmr-request-id': 'abcd-1234-efgh-5678'
-      }) // TODO: I put this uuid here since it is getting passed from the context
+      })
 
     await expect(
       graphDbDuplicateCollectionsDatasource(
@@ -172,7 +171,8 @@ describe('graphDb', () => {
             doi: 'mock doi'
           }
         },
-        { 'CMR-Request-Id': 'abcd-1234-efgh-5678' }
+        { 'CMR-Request-Id': 'abcd-1234-efgh-5678' },
+        'someEdlUsername'
       )
     ).rejects.toThrow(Error)
   })
--- src/utils/__tests__/verifyEDLJwt.test.js
+++ src/utils/__tests__/verifyEDLJwt.test.js
@@ -3,41 +3,29 @@ import { verifyEDLJwt } from '../verifyJwtWithPublicKey'
 
 const OLD_ENV = process.env
 
+beforeEach(() => {
+  process.env = { ...OLD_ENV }
+  // This JWK is from a local instance of EDL it is NOT utilzied by any cloud edl systems
+  process.env.rsaKey = '{"keys":[{"kty":"RSA","n":"xSxiOkM8m8oCyWn-sNNZxBVTUcPAlhXRjKpLTYIM21epMC9rqEnrgL7iuntmp3UcffOIKtFHOtCG-jWUkyzxZHPPMo0kYZVHKRjGj-AVAy3FA-d2AtUc1dPlrQ0TpdDoTzew_6-48BcbdFEQI3161wcMoy40unYYYfzo3KuUeNcCY3cmHzSkYn4iQHaBy5zTAzKTIcYCTpaBGDk4_IyuysvaYmgwdeNO26hNV9dmPx_rWgYZYlashXZ_kRLirDaGpnJJHyPrYaEJpMIWuIfsh_UoMjsyuoQGe4XU6pG8uNnUd31mHa4VU78cghGZGrCz_YkPydfFlaX65LBp9aLdCyKkA66pDdnCkm8odVMgsH2x_kGM7sNlQ6ELTsT-dtJoiEDI_z3fSZehLw469QpTGQjfsfXUCYm8QrGckJF4bJc935TfGU86qr2Ik2YoipP_L4K_oqUf8i6bwO0iomo_C7Ukr4l-dh4D5O7szAb9Ga804OZusFk3JENlc1-RlB20S--dWrrO-v_L8WI2d72gizOKky0Xwzd8sseEqfMWdktyeKoaW0ANkBJHib4E0QxgedeTca0DH_o0ykMjOZLihOFtvDuCsbHG3fv41OQr4qRoX97QO2Hj1y3EBYtUEypan46g-fUyLCt-sYP66RkBYzCJkikCbzF_ECBDgX314_0","e":"AQAB","kid":"edljwtpubkey_development"}]}'
+})
+
+afterEach(() => {
+  process.env = OLD_ENV
+})
+
 describe('verifyEDLJwt', () => {
-  const jwks = createJWKSMock('https://MYAUTH0APP.auth0.com/')
-
-  test('JWT token testing if there is no Kid value provided and so it deafaults to empty string', async () => {
-    process.env = { ...OLD_ENV }
-
-    // This Rsa Key is from a local instance of EDL it is not a key on any cloud system
-    process.env.rsaKey = '{"keys":[{"kty":"RSA","n":"xSxiOkM8m8oCyWn-sNNZxBVTUcPAlhXRjKpLTYIM21epMC9rqEnrgL7iuntmp3UcffOIKtFHOtCG-jWUkyzxZHPPMo0kYZVHKRjGj-AVAy3FA-d2AtUc1dPlrQ0TpdDoTzew_6-48BcbdFEQI3161wcMoy40unYYYfzo3KuUeNcCY3cmHzSkYn4iQHaBy5zTAzKTIcYCTpaBGDk4_IyuysvaYmgwdeNO26hNV9dmPx_rWgYZYlashXZ_kRLirDaGpnJJHyPrYaEJpMIWuIfsh_UoMjsyuoQGe4XU6pG8uNnUd31mHa4VU78cghGZGrCz_YkPydfFlaX65LBp9aLdCyKkA66pDdnCkm8odVMgsH2x_kGM7sNlQ6ELTsT-dtJoiEDI_z3fSZehLw469QpTGQjfsfXUCYm8QrGckJF4bJc935TfGU86qr2Ik2YoipP_L4K_oqUf8i6bwO0iomo_C7Ukr4l-dh4D5O7szAb9Ga804OZusFk3JENlc1-RlB20S--dWrrO-v_L8WI2d72gizOKky0Xwzd8sseEqfMWdktyeKoaW0ANkBJHib4E0QxgedeTca0DH_o0ykMjOZLihOFtvDuCsbHG3fv41OQr4qRoX97QO2Hj1y3EBYtUEypan46g-fUyLCt-sYP66RkBYzCJkikCbzF_ECBDgX314_0","e":"AQAB"}]}'
-
-    // This token is from a local instance of EDL it is NOT a bearer token utilzied by cloud edl systems
-    const token = 'eyJ0eXAiOiJKV1QiLCJvcmlnaW4iOiJFYXJ0aGRhdGEgTG9naW4iLCJzaWciOiJlZGxqd3RwdWJrZXlfZGV2ZWxvcG1lbnQiLCJhbGciOiJSUzI1NiJ9.eyJ0eXBlIjoiVXNlciIsImNsaWVudF9pZCI6ImxvY2FsZGV2IiwiZXhwIjoxMTY2MTU0NzA5NCwiaWF0IjoxNjYxNTQ3MDk0LCJpc3MiOiJFYXJ0aGRhdGEgTG9naW4iLCJ1aWQiOiJtc3RhcnR6ZWx0ZXN0ZXIifQ.tAA4zu2K3NFJVX96_M1qb1lrDQ1l7uMjMWNE5Jkxw16RnHTgIt6dcEdX-YcXUbJqKDU39RLcB_O8hgzd9e7pHBdxrwhjl4LnuNcSj1XZGTLh-RD5VW1jRkQe_pcH1noniUqkXaYuVGUybnbhDZa_37Oxs-vr0lT06Qk82elV5Y1dq_YLQeABv4O0BiktpPoCSjSIfTW6jEUS-ONk07r5N5O_Me7H-QbPhEtiax1N3zcTRWyNn7lM6vQxl7d-ywRKqQaeA9Iy-ufmGXoczLvvN4HsaKbVhQ_llw6Xnj7cKpd4WJ6VABDETlMlcjwtyvSt-q3aToy9N4_EkMGQbxkbsQrvf9LI2VM7J799uhW9E4VvOEl-CafkFnOgjo1nvMpq3fZq1zIfG4eA6UrYpQQz_gcdFfoL-p5ZI_BbMO0PK_8XAfE8O0w7b7i7QJ_EmKKUA2QibJLK8qdlOhbLNu6ORTyqvxbawMAjW_ZzJIZnDwjyuIoJNBFJQxiz2SMBdQwAuJDGcyzIGEAheF0ffB-mJG28HyVvhbjQhP2ByE0mZoDFhqmgk47FnQNFL7mTdtSbI-KvOXb3rBEaELUdWDuqjgnOxQehJzFlbqETRZfZDuEUq7q1Zl227k2178lvVVPQuco8Auo180qVaJcAs9Fd2k-i6oNkalC6MNjgmEpBUSE'
-    const passToken = `Bearer ${token}`
-    const returnObject = await verifyEDLJwt(passToken)
-
-    // This is an example uid from edl only
-    expect(returnObject).toEqual('mstartzeltester')
-    process.env = OLD_ENV
-    jest.resetModules()
-  })
-
   test('Verifies a correct jwt token', async () => {
-    process.env = { ...OLD_ENV }
-    process.env.rsaKey = '{"keys":[{"kty":"RSA","n":"xSxiOkM8m8oCyWn-sNNZxBVTUcPAlhXRjKpLTYIM21epMC9rqEnrgL7iuntmp3UcffOIKtFHOtCG-jWUkyzxZHPPMo0kYZVHKRjGj-AVAy3FA-d2AtUc1dPlrQ0TpdDoTzew_6-48BcbdFEQI3161wcMoy40unYYYfzo3KuUeNcCY3cmHzSkYn4iQHaBy5zTAzKTIcYCTpaBGDk4_IyuysvaYmgwdeNO26hNV9dmPx_rWgYZYlashXZ_kRLirDaGpnJJHyPrYaEJpMIWuIfsh_UoMjsyuoQGe4XU6pG8uNnUd31mHa4VU78cghGZGrCz_YkPydfFlaX65LBp9aLdCyKkA66pDdnCkm8odVMgsH2x_kGM7sNlQ6ELTsT-dtJoiEDI_z3fSZehLw469QpTGQjfsfXUCYm8QrGckJF4bJc935TfGU86qr2Ik2YoipP_L4K_oqUf8i6bwO0iomo_C7Ukr4l-dh4D5O7szAb9Ga804OZusFk3JENlc1-RlB20S--dWrrO-v_L8WI2d72gizOKky0Xwzd8sseEqfMWdktyeKoaW0ANkBJHib4E0QxgedeTca0DH_o0ykMjOZLihOFtvDuCsbHG3fv41OQr4qRoX97QO2Hj1y3EBYtUEypan46g-fUyLCt-sYP66RkBYzCJkikCbzF_ECBDgX314_0","e":"AQAB","kid":"edljwtpubkey_development"}]}'
     // This token is from a local instance of EDL it is NOT a bearer token utilzied by cloud edl systems
     const token = 'eyJ0eXAiOiJKV1QiLCJvcmlnaW4iOiJFYXJ0aGRhdGEgTG9naW4iLCJzaWciOiJlZGxqd3RwdWJrZXlfZGV2ZWxvcG1lbnQiLCJhbGciOiJSUzI1NiJ9.eyJ0eXBlIjoiVXNlciIsImNsaWVudF9pZCI6ImxvY2FsZGV2IiwiZXhwIjoxMTY2MTU0NzA5NCwiaWF0IjoxNjYxNTQ3MDk0LCJpc3MiOiJFYXJ0aGRhdGEgTG9naW4iLCJ1aWQiOiJtc3RhcnR6ZWx0ZXN0ZXIifQ.tAA4zu2K3NFJVX96_M1qb1lrDQ1l7uMjMWNE5Jkxw16RnHTgIt6dcEdX-YcXUbJqKDU39RLcB_O8hgzd9e7pHBdxrwhjl4LnuNcSj1XZGTLh-RD5VW1jRkQe_pcH1noniUqkXaYuVGUybnbhDZa_37Oxs-vr0lT06Qk82elV5Y1dq_YLQeABv4O0BiktpPoCSjSIfTW6jEUS-ONk07r5N5O_Me7H-QbPhEtiax1N3zcTRWyNn7lM6vQxl7d-ywRKqQaeA9Iy-ufmGXoczLvvN4HsaKbVhQ_llw6Xnj7cKpd4WJ6VABDETlMlcjwtyvSt-q3aToy9N4_EkMGQbxkbsQrvf9LI2VM7J799uhW9E4VvOEl-CafkFnOgjo1nvMpq3fZq1zIfG4eA6UrYpQQz_gcdFfoL-p5ZI_BbMO0PK_8XAfE8O0w7b7i7QJ_EmKKUA2QibJLK8qdlOhbLNu6ORTyqvxbawMAjW_ZzJIZnDwjyuIoJNBFJQxiz2SMBdQwAuJDGcyzIGEAheF0ffB-mJG28HyVvhbjQhP2ByE0mZoDFhqmgk47FnQNFL7mTdtSbI-KvOXb3rBEaELUdWDuqjgnOxQehJzFlbqETRZfZDuEUq7q1Zl227k2178lvVVPQuco8Auo180qVaJcAs9Fd2k-i6oNkalC6MNjgmEpBUSE'
     const passToken = `Bearer ${token}`
     const returnObject = await verifyEDLJwt(passToken)
 
-    // This is an example uid from edl only
+    // This is an example uid from a local instance of edl only
     expect(returnObject).toEqual('mstartzeltester')
-    process.env = OLD_ENV
-    jest.resetModules()
   })
 
   test('Given a bad token console the error and return an empty list for the decoded token', async () => {
+    const jwks = createJWKSMock('https://MYAUTH0APP.auth0.com/')
     jwks.start()
     const consoleMock = jest.spyOn(console, 'log').mockImplementation(() => jest.fn())
 
@@ -53,6 +41,5 @@ describe('verifyEDLJwt', () => {
     )
     expect(returnObject).toEqual(decodedTestJwtUserId)
     jwks.stop()
-    jest.resetModules()
   })
 })
