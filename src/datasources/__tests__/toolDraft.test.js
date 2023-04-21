2c755e841a8dec35761647d797f1f437c2037d4a
Deep Mistry <deep.mistry@nasa.gov>
2023-04-21 15:32:15 -0400

    CMR-9085: Added test cases for ToolDraft

---
+++ src/datasources/__tests__/toolDraft.test.js
@@ -0,0 +1,99 @@
+import nock from 'nock'
+
+import toolDraftDataSource from '../toolDraft'
+
+let requestInfo
+
+describe('toolDraft', () => {
+  const OLD_ENV = process.env
+
+  beforeEach(() => {
+    jest.resetAllMocks()
+
+    jest.restoreAllMocks()
+
+    process.env = { ...OLD_ENV }
+
+    process.env.mmtRootUrl = 'http://example.com'
+
+    // Default requestInfo
+    requestInfo = {
+      name: 'toolDraft',
+      alis: 'toolDraft',
+      args: {},
+      fieldsByTypeName: {
+        ToolDraft: {
+          longName: {
+            name: 'longName',
+            alis: 'longName',
+            args: {},
+            fieldsByTypeName: {}
+          },
+          description: {
+            name: 'description',
+            alis: 'description',
+            args: {},
+            fieldsByTypeName: {}
+          }
+        }
+      }
+    }
+  })
+
+  afterEach(() => {
+    process.env = OLD_ENV
+  })
+
+  test('return the tool draft results', async () => {
+    nock(/example/)
+      .defaultReplyHeaders({
+        'X-Request-Id': 'abcd-1234-efgh-5678'
+      })
+      .get(/api/)
+      .reply(200, {
+        draft: {
+          LongName: 'Mock Long Name',
+          Description: 'Mock Description'
+        }
+      })
+
+    const response = await toolDraftDataSource({
+      params: {
+        id: '123'
+      }
+    }, {
+      headers: {
+        'Client-Id': 'mmt-react-ui',
+        'X-Request-Id': 'abcd-1234-efgh-5678'
+      }
+    }, requestInfo)
+
+    expect(response).toEqual([{
+      longName: 'Mock Long Name',
+      description: 'Mock Description'
+    }])
+  })
+
+  test('catches errors received from mmtQuery', async () => {
+    nock(/example/)
+      .get(/api/)
+      .reply(500, {
+        errors: ['HTTP Error']
+      }, {
+        'cmr-request-id': 'abcd-1234-efgh-5678'
+      })
+
+    await expect(
+      toolDraftDataSource({
+        params: {
+          id: '123'
+        }
+      }, {
+        headers: {
+          'Client-Id': 'mmt-react-ui',
+          'X-Request-Id': 'abcd-1234-efgh-5678'
+        }
+      }, requestInfo)
+    ).rejects.toThrow(Error)
+  })
+})
---
+++ src/resolvers/__tests__/toolDraft.test.js
@@ -0,0 +1,90 @@
+import nock from 'nock'
+
+import {
+  buildContextValue,
+  server
+} from './__mocks__/mockServer'
+
+const contextValue = buildContextValue()
+
+describe('Collection', () => {
+  const OLD_ENV = process.env
+
+  beforeEach(() => {
+    process.env = { ...OLD_ENV }
+
+    process.env.mmtRootUrl = 'http://example.com'
+  })
+
+  afterEach(() => {
+    process.env = OLD_ENV
+  })
+
+  describe('Query', () => {
+    describe('toolDraft', () => {
+      describe('with results', () => {
+        test('returns results', async () => {
+          nock(/example/)
+            .defaultReplyHeaders({
+              'X-Request-Id': 'abcd-1234-efgh-5678'
+            })
+            .get(/api/)
+            .reply(200, {
+              draft: {
+                DOI: 'Mock doi'
+              }
+            })
+
+          const response = await server.executeOperation({
+            variables: {},
+            query: `{
+              toolDraft (params: { id: 123 }) {
+                doi
+              }
+            }`
+          }, {
+            contextValue
+          })
+
+          const { data } = response.body.singleResult
+
+          expect(data).toEqual({
+            toolDraft: {
+              doi: 'Mock doi'
+            }
+          })
+        })
+      })
+
+      describe('with no results', () => {
+        test('returns no results', async () => {
+          nock(/example/)
+            .defaultReplyHeaders({
+              'X-Request-Id': 'abcd-1234-efgh-5678'
+            })
+            .get(/api/)
+            .reply(200, {})
+
+          const response = await server.executeOperation({
+            variables: {},
+            query: `{
+              toolDraft (params: { id: 123 }) {
+                doi
+              }
+            }`
+          }, {
+            contextValue
+          })
+
+          const { data } = response.body.singleResult
+
+          expect(data).toEqual({
+            toolDraft: {
+              doi: null
+            }
+          })
+        })
+      })
+    })
+  })
+})
