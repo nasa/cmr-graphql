cc1ca3d2b90b91b1bbf72851f9709b0aeb36941d
Deep Mistry <deep.mistry@nasa.gov>
2023-05-02 16:02:53 -0400

    CMR-9088: Cleanup

--- src/datasources/variableDraft.js
+++ src/datasources/variableDraft.js
@@ -10,12 +10,13 @@ export default async (params, context, parsedInfo) => {
   const requestInfo = parseRequestedFields(parsedInfo, variableDraftKeyMap, 'variableDraft')
 
   const variableDraft = new DraftConecpt(headers, requestInfo, params, 'variableDraft')
+
   // Query MMT
   variableDraft.fetch(params)
 
   // Parse the response from MMT
   await variableDraft.parse(requestInfo, params)
-  console.log('*** var', variableDraft.parse(requestInfo, params))
+
   // Return a formatted JSON response
   return variableDraft.getFormattedResponse()
 }
