81745f010f39f0c9537d62493fb7b362f893b00c
Deep Mistry <deep.mistry@nasa.gov>
2023-05-02 14:36:23 -0400

    CMR-9088: Added test cases

--- src/datasources/variableDraft.js
+++ src/datasources/variableDraft.js
@@ -2,21 +2,20 @@ import { parseRequestedFields } from '../utils/parseRequestedFields'
 
 import variableDraftKeyMap from '../utils/umm/variableDraftKeyMap.json'
 
-import VariableDraft from '../cmr/concepts/variableDraft'
+import DraftConecpt from '../cmr/concepts/draftConcept'
 
 export default async (params, context, parsedInfo) => {
   const { headers } = context
 
   const requestInfo = parseRequestedFields(parsedInfo, variableDraftKeyMap, 'variableDraft')
 
-  const variableDraft = new VariableDraft(headers, requestInfo, params)
-
+  const variableDraft = new DraftConecpt(headers, requestInfo, params, 'variableDraft')
   // Query MMT
   variableDraft.fetch(params)
 
   // Parse the response from MMT
   await variableDraft.parse(requestInfo, params)
-
+  console.log('*** var', variableDraft.parse(requestInfo, params))
   // Return a formatted JSON response
   return variableDraft.getFormattedResponse()
 }
