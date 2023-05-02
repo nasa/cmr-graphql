415f76f64376eadd167f42b42fd2ca111d711d16
Deep Mistry <deep.mistry@nasa.gov>
2023-05-02 14:57:40 -0400

    CMR-9085: Fixed couple spellings

--- src/datasources/collectionDraft.js
+++ src/datasources/collectionDraft.js
@@ -2,14 +2,14 @@ import { parseRequestedFields } from '../utils/parseRequestedFields'
 
 import collectionDraftKeyMap from '../utils/umm/collectionDraftKeyMap.json'
 
-import DraftConecpt from '../cmr/concepts/draftConcept'
+import DraftConcept from '../cmr/concepts/draftConcept'
 
 export default async (params, context, parsedInfo) => {
   const { headers } = context
 
   const requestInfo = parseRequestedFields(parsedInfo, collectionDraftKeyMap, 'collectionDraft')
 
-  const collectionDraft = new DraftConecpt(headers, requestInfo, params, 'collectionDraft')
+  const collectionDraft = new DraftConcept(headers, requestInfo, params, 'collectionDraft')
 
   // Query MMT
   collectionDraft.fetch(params)
