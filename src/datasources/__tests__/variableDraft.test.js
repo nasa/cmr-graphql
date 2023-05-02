04164b67fec6ea41057bf136f887b9148f307651
Deep Mistry <deep.mistry@nasa.gov>
2023-05-02 14:36:23 -0400

    CMR-9088: Added test cases

---
+++ src/datasources/__tests__/variableDraft.test.js
@@ -0,0 +1,67 @@
+import nock from 'nock'
+
+import variableDraftDataSource from '../variableDraft'
+
+let requestInfo
+
+describe('variableDraft', () => {
+  const OLD_ENV = process.env
+  beforeEach(() => {
+    jest.resetAllMocks()
+
+    jest.restoreAllMocks()
+
+    process.env = { ...OLD_ENV }
+
+    process.env.mmtRootUrl = 'http://example.com'
+
+    // Default requestInfo
+    requestInfo = {
+      name: 'variableDraft',
+      alis: 'variableDraft',
+      args: {},
+      fieldsByTypeName: {
+        VariableDraft: {
+          longName: {
+            name: 'longName',
+            alis: 'longName',
+            args: {},
+            fieldsByTypeName: {}
+          }
+        }
+      }
+    }
+  })
+
+  afterEach(() => {
+    process.env = OLD_ENV
+  })
+
+  test('return the variable draft results', async () => {
+    nock(/example/)
+      .defaultReplyHeaders({
+        'X-Request-Id': 'abcd-1234-efgh-5678'
+      })
+      .get(/api\/drafts/)
+      .reply(200, {
+        draft: {
+          LongName: 'Mock Long Name'
+        }
+      })
+
+    const response = await variableDraftDataSource({
+      params: {
+        id: '123'
+      }
+    }, {
+      headers: {
+        'Client-Id': 'eed-test-graphql',
+        'X-Request-Id': 'abcd-1234-efgh-5678'
+      }
+    }, requestInfo)
+
+    expect(response).toEqual([{
+      longName: 'Mock Long Name'
+    }])
+  })
+})
