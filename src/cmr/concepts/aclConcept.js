c2fd115cc91858e3534db8b1fc15c3873cfb8c8a
tle1989 <tle1989@yahoo.com>
2024-02-08 16:40:57 -0500

    GQL-21: Removed repeated codes by aclConcept.js extends Concept.js

--- src/cmr/concepts/aclConcept.js
+++ src/cmr/concepts/aclConcept.js
@@ -1,15 +1,11 @@
 import snakeCaseKeys from 'snakecase-keys'
-
 import { pick, snakeCase } from 'lodash'
-
-import { CONCEPT_TYPES } from '../../constants'
-
 import { aclQuery } from '../../utils/aclQuery'
 import { mergeParams } from '../../utils/mergeParams'
-import { parseError } from '../../utils/parseError'
 import { downcaseKeys } from '../../utils/downcaseKeys'
+import Concept from './concept'
 
-export default class AclConcept {
+export default class AclConcept extends Concept {
   /**
    * Instantiates an AclConcept object
    * @param {String} conceptType The CMR concept type to query against
@@ -18,6 +14,8 @@ export default class AclConcept {
    * @param {Object} params GraphQL query parameters
    */
   constructor(conceptType, headers = {}, requestInfo = {}, params = {}) {
+    super(conceptType, headers, requestInfo, params)
+
     // Set properties for data available during instantiation
     this.conceptType = conceptType
     this.headers = headers
@@ -57,20 +55,6 @@ export default class AclConcept {
   }
 
   /**
-   * Log requested keys for metrics and debugging
-   * @param {Array} keys List of keys being requested
-   * @param {String} format Format of the request (json, umm, meta)
-   */
-  logKeyRequest(keys, format) {
-    // Prevent logging concept types, their meta keys are logged above
-    const filteredKeys = keys.filter((field) => CONCEPT_TYPES.indexOf(field) === -1)
-
-    filteredKeys.forEach((key) => {
-      console.log(`Request ${this.getRequestId()} from ${this.getClientId()} to [concept: ${this.getConceptType()}] requested [format: ${format}, key: ${key}]`)
-    })
-  }
-
-  /**
    * Query the CMR JSON API endpoint to retrieve requested data
    * @param {Object} searchParams Parameters provided by the query
    * @param {Array} requestedKeys Keys requested by the query
@@ -86,127 +70,6 @@ export default class AclConcept {
       nonIndexedKeys: this.getNonIndexedKeys(),
       headers: providedHeaders
     })
-  }
-
-  /**
-   * Return the response from the query to CMR
-   */
-  async getResponse() {
-    return this.response
-  }
-
-  /**
-   * Set the total number of records available for a given search to the json endpoint
-   * @param {Integer} itemCount The total number of records available for a given search
-   */
-  setJsonItemCount(itemCount) {
-    this.jsonItemCount = parseInt(itemCount, 10)
-  }
-
-  /**
-   * Parse and return the array of data from the nested response body
-   * @param {Object} jsonResponse HTTP response from the CMR endpoint
-   */
-  parseJsonBody(jsonResponse) {
-    const { data } = jsonResponse
-    const { items } = data
-
-    return items
-  }
-
-  /**
-   * Rename fields, add fields, modify fields, etc
-   * @param {Object} item The item returned from the CMR json endpoint
-   */
-  normalizeJsonItem(item) {
-    return item
-  }
-
-  /**
-   * Set the search after identifier for a search against against the json endpoint
-   * @param {Float} searchAfterIdentifier The search after identifier provided by CMR
-   */
-  setJsonSearchAfter(searchAfterIdentifier) {
-    this.jsonSearchAfterIdentifier = searchAfterIdentifier
-  }
-
-  /**
-   * Retrieve the request id header from the request
-   * @param {Object} headers The provided headers from the query
-   * @return {String} Request ID defined in the headers
-   */
-  getRequestId() {
-    const {
-      'CMR-Request-Id': requestId
-    } = this.headers
-
-    return requestId
-  }
-
-  /**
-   * Retrieve the client id header from the request
-   * @param {Object} headers The provided headers from the query
-   * @return {String} Client ID defined in the headers
-   */
-  getClientId() {
-    const {
-      'Client-Id': clientId
-    } = this.headers
-
-    return clientId
-  }
-
-  /**
-   * Get the CMR concept type of this object
-   */
-  getConceptType() {
-    return this.conceptType
-  }
-
-  /**
-   * Returns an array of keys that should not be indexed when sent to CMR
-   */
-  getNonIndexedKeys() {
-    return [
-      'concept_id',
-      'offset',
-      'page_size',
-      'sort_key',
-      'permitted_user',
-      'include_full_acl',
-      'page_num',
-      'target'
-    ]
-  }
-
-  /**
-   * Retrieve a single item from the result set
-   * @param {String} id The concept id of an item to lookup in the result set
-   */
-  getItem(id) {
-    // If an item with the provided id does not exist, create one
-    if (Object.keys(this.items).indexOf(id) === -1) {
-      this.items[id] = {}
-    }
-
-    // Fetch the item from the result set
-    const { [id]: item } = this.items
-
-    return item
-  }
-
-  /**
-   * Set a specific property on an object within the result set of a search operation
-   * @param {String} id Concept ID to set a value for within the result set
-   * @param {String} key The key to set within the result
-   * @param {Any} value They value to assign to the key within the result
-   */
-  setItemValue(id, key, value) {
-    // Ensure that our object knows about the item by creating it if it doesn't exist
-    this.getItem(id)
-
-    // Set the value
-    this.items[id][key] = value
   }
 
   /**
@@ -244,13 +107,6 @@ export default class AclConcept {
   }
 
   /**
-   * Return the raw result set
-   */
-  getItems() {
-    return this.items
-  }
-
-  /**
    * Get the total number of records available for a given search across all endpoints. Also
    * ensure that the value is the same if a given search spans multiple endpoints
    */
@@ -263,80 +119,5 @@ export default class AclConcept {
     }
 
     return 0
-  }
-
-  /**
-   * Encode and return a base64 hashed version of the json and umm search after identifier
-   */
-  encodeCursor() {
-    if (this.jsonSearchAfterIdentifier || this.ummSearchAfterIdentifier) {
-      return Buffer.from(
-        JSON.stringify({
-          json: this.jsonSearchAfterIdentifier,
-          umm: this.ummSearchAfterIdentifier
-        })
-      ).toString('base64')
-    }
-
-    return null
-  }
-
-  getFormattedResponse() {
-    // Determine if the query was a list or not, list queries return meta
-    // keys using a slightly different format
-    const {
-      isList
-    } = this.requestInfo
-
-    // Retrieve the result set regardless of whether or not the query is a list or not
-    const items = this.getItems()
-
-    if (isList) {
-      const count = this.getItemCount()
-      const cursor = this.encodeCursor()
-
-      return {
-        count,
-        cursor,
-        items: Object.values(items)
-      }
-    }
-
-    return Object.values(items)
-  }
-
-  /**
-   * Returns an array of keys representing supported search params for the json endpoint
-   */
-  getPermittedJsonSearchParams() {
-    return [
-      'concept_id',
-      'offset',
-      'page_size',
-      'sort_key',
-      'permitted_user',
-      'include_full_acl',
-      'page_num',
-      'target'
-    ]
-  }
-
-  /**
-   * Parses the response from each endpoint after a request is made
-   * @param {Object} requestInfo Parsed data pertaining to the Graph query
-   */
-  async parse(requestInfo) {
-    try {
-      const {
-        jsonKeys
-      } = requestInfo
-
-      const response = await this.getResponse()
-
-      const [jsonResponse] = response
-      await this.parseJson(jsonResponse, jsonKeys)
-    } catch (e) {
-      parseError(e, { reThrowError: true })
-    }
   }
 }
