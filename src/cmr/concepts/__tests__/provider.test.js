c458c2558a5fabeac04337e5924a806e521c1f17
Trevor Lang <trevorlang@ymail.com>
2024-03-14 10:36:24 -0400

    MMT-3493: Adding providers query (#100)
    * MMT-3493: Adding providers query
    
    * MMT-3493: PR Feedback

---
+++ src/cmr/concepts/__tests__/provider.test.js
@@ -0,0 +1,55 @@
+import nock from 'nock'
+
+import { parseRequestedFields } from '../../../utils/parseRequestedFields'
+import Provider from '../provider'
+import providerKeyMap from '../../../utils/umm/providerKeyMap.json'
+
+process.env.cmrRootUrl = 'http://example.com'
+
+describe('Provider concept', () => {
+  beforeEach(() => {
+    jest.resetAllMocks()
+
+    jest.restoreAllMocks()
+  })
+
+  describe('when fetching', () => {
+    test('requests the json', async () => {
+      nock('http://example.com')
+        .get('/ingest/providers')
+        .reply(200, { data: 'test' })
+
+      const parsedInfo = {
+        name: 'provider',
+        alias: 'providers',
+        args: {},
+        fieldsByTypeName: {
+          ProviderList: {
+            count: {},
+            items: {}
+          }
+        }
+      }
+
+      const requestInfo = parseRequestedFields(parsedInfo, providerKeyMap, 'provider')
+
+      const provider = new Provider({}, requestInfo, {})
+
+      await provider.fetch({
+        params: {
+          keyword: 'test',
+          limit: 5,
+          offset: 0,
+          provider: null,
+          sortKey: null
+        },
+        pageSize: 5
+      })
+
+      const response = await provider.response
+
+      expect(await response[0].config.data).toEqual(undefined)
+      expect(await response[0].config.url).toEqual('http://example.com/ingest/providers')
+    })
+  })
+})
--- src/graphql/handler.js
+++ src/graphql/handler.js
@@ -22,6 +22,7 @@ import graphDbSource from '../datasources/graphDb'
 import gridSource from '../datasources/grid'
 import maxItemsPerOrderSource from '../datasources/maxItemsPerOrder'
 import orderOptionSource from '../datasources/orderOption'
+import providerSource from '../datasources/provider'
 import serviceDraftSource from '../datasources/serviceDraft'
 import tagDefinitionSource from '../datasources/tagDefinition'
 import toolDraftSource from '../datasources/toolDraft'
@@ -173,6 +174,7 @@ export default startServerAndCreateLambdaHandler(
           gridSource,
           maxItemsPerOrderSource,
           orderOptionSource,
+          providerSource,
           serviceDraftSource,
           serviceSourceDelete,
           serviceSourceFetch,
