6dd440a29453ffada2aa3b4a25a98117c4603181
Matthew Crouch <matthew@element84.com>
2024-05-02 16:15:57 -0400

    GQL-42: Adds a rule to allow the local MMT Admin to pass the new permissions

--- src/permissions/__tests__/permissions.test.js
+++ src/permissions/__tests__/permissions.test.js
@@ -1,10 +1,12 @@
-import { shield } from 'graphql-shield'
+import { race, shield } from 'graphql-shield'
 
 // We don't have to call this file, simply importing it will call the graphql-shield shield function
 // eslint-disable-next-line no-unused-vars
 import permissions from '../index'
+
 import { canReadSystemGroups } from '../acls/canReadSystemGroups'
 import { canCreateSystemGroups } from '../acls/canCreateSystemGroups'
+import { isLocalMMTAdmin } from '../rules/isLocalMMTAdmin'
 
 vi.mock('graphql-shield', async () => {
   const original = await vi.importActual('graphql-shield')
@@ -20,13 +22,28 @@ describe('permissions', () => {
     expect(shield).toHaveBeenCalledTimes(1)
     expect(shield.mock.calls[0][0]).toEqual({
       Query: {
-        group: canReadSystemGroups,
-        groups: canReadSystemGroups
+        group: race(
+          isLocalMMTAdmin,
+          canReadSystemGroups
+        ),
+        groups: race(
+          isLocalMMTAdmin,
+          canReadSystemGroups
+        )
       },
       Mutation: {
-        createGroup: canCreateSystemGroups,
-        deleteGroup: canCreateSystemGroups,
-        updateGroup: canCreateSystemGroups
+        createGroup: race(
+          isLocalMMTAdmin,
+          canCreateSystemGroups
+        ),
+        deleteGroup: race(
+          isLocalMMTAdmin,
+          canCreateSystemGroups
+        ),
+        updateGroup: race(
+          isLocalMMTAdmin,
+          canCreateSystemGroups
+        )
       }
     })
   })
--- src/permissions/index.js
+++ src/permissions/index.js
@@ -1,24 +1,46 @@
-import { allow, shield } from 'graphql-shield'
+import {
+  allow,
+  race,
+  shield
+} from 'graphql-shield'
 
 import { canReadSystemGroups } from './acls/canReadSystemGroups'
 import { canCreateSystemGroups } from './acls/canCreateSystemGroups'
+import { isLocalMMTAdmin } from './rules/isLocalMMTAdmin'
 
 const permissions = shield(
   {
     Query: {
-      group: canReadSystemGroups,
-      groups: canReadSystemGroups
+      // The `race` rule allows you to chain the rules so that execution stops once one of them returns.
+      // So if the `isLocalMMTAdmin` rule passes, don't bother checking the permissions of the user
+      group: race(
+        isLocalMMTAdmin,
+        canReadSystemGroups
+      ),
+      groups: race(
+        isLocalMMTAdmin,
+        canReadSystemGroups
+      )
     },
     Mutation: {
-      createGroup: canCreateSystemGroups,
-      deleteGroup: canCreateSystemGroups,
-      updateGroup: canCreateSystemGroups
+      createGroup: race(
+        isLocalMMTAdmin,
+        canCreateSystemGroups
+      ),
+      deleteGroup: race(
+        isLocalMMTAdmin,
+        canCreateSystemGroups
+      ),
+      updateGroup: race(
+        isLocalMMTAdmin,
+        canCreateSystemGroups
+      )
     }
   },
   {
     fallbackRule: allow,
 
-    // Only allow the external errors (like Prisma errors) when running in the local DEV environment
+    // Only allow the external errors when running in the local DEV environment
     allowExternalErrors: process.env.IS_OFFLINE,
 
     // `fallbackError` displays a useful message to the user containing a requestId, rather than the default "Not Authorized!" GraphQL Shield error.
