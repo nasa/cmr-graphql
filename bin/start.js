56e4fa61204b889ca4eceb9c2ca6e4f8c6f8b0c7
Matthew Crouch <mcrouch@innovim.com>
2025-01-03 15:44:16 -0500

    GQL-98: Fixes start command

--- bin/start.js
+++ bin/start.js
@@ -4,7 +4,7 @@ concurrently([{
   command: 'npm run watch',
   name: 'watch'
 }, {
-  command: 'sam local start-api -t ./cdk/earthdata-access/cdk.out/earthdata-access-dev.template.json --warm-containers LAZY --port 5001 --docker-network host',
+  command: 'sam local start-api -t ./cdk/graphql/cdk.out/graphql-dev.template.json --warm-containers LAZY --port 3013 --docker-network host',
   name: 'api'
 }], {
   prefix: 'name',
